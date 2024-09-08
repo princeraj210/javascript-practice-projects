da8ff1c219b0ad3e13d0f0cc82a081ba45b128b4
Prince Chaubey <154663868+princeraj210@users.noreply.github.com>
2024-09-08 23:28:03 +0530

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,19 @@
+let userInput = document.getElementById("userInput");
+let expression = '';
+
+function press(num) {
+    expression += num;
+    userInput.value = expression;
+
+}
+
+function equal() {
+    userInput.value = eval(expression);
+    expression = '';
+
+}
+
+function erase() {
+    expression = '';
+    userInput.value = expression;
+}
